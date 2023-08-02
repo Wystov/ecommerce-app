4eadb6ce321b3ae6dd0b14888856ce519cfa5ade
Wystov <wystov@gmail.com>
2023-08-02 18:57:18 +0300

    chore: typescript config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,13 +10,23 @@ module.exports = {
     '@vue/eslint-config-prettier/skip-formatting',
     '@vue/eslint-config-airbnb',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
+    project: './tsconfig.app.json',
+    tsconfigRootDir: __dirname,
   },
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-plusplus': 'off',
     'import/prefer-default-export': 'off',
     'max-lines-per-function': ['error', 40],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
     'import/extensions': [
       'error',
       'ignorePackages',
