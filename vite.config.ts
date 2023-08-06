e0fd94e997f63067f7172b04b09b603277c19b15
Wystov <wystov@gmail.com>
2023-08-06 23:00:02 +0300

    feat: configured build for development

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
   ],
@@ -12,4 +12,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+  build: {
+    minify: mode === 'production' ? 'esbuild' : false,
+    sourcemap: mode === 'production' ? false : 'inline',
+  },
+}));
