6492f3ab3462877dd240dc6173ae8d046b5ff428
Duxcoder <iinegve_25@mail.ru>
2023-08-08 07:10:27 +0300

    feat: create files for types components

---
+++ src/types/components/types.ts
@@ -0,0 +1,14 @@
+interface ButtonClasses {
+  'theme-dark': boolean;
+  'theme-light': boolean;
+  'size-small': boolean;
+  'size-medium': boolean;
+  'size-large': boolean;
+  link: boolean;
+  rounded: boolean;
+  circle: boolean;
+  active: boolean;
+  disabled: boolean;
+}
+
+export type { ButtonClasses };
