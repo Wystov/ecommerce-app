c9e124bb4ecd0cce2b8a282838169caa66b8a7ab
Anna Coração <lazakovich@gmail.com>
2023-08-08 20:58:23 +0300

    feat: add base input component and style.css file for base styles

--- src/components/shared/BaseInput.vue
+++ src/components/shared/BaseInput.vue
@@ -1,26 +1,92 @@
 <template>
-  <label for="inputField">Enter:</label>
-  <input type="text" id="inputField">
-  <!-- <label for="inputField">{{ labelText }}</label>
-  <input v-model.trim="inputValue" type="text" id="inputField"> -->
+  <div class="inputBlock">
+    <label for="inputField" class="label" :class="{ darkThemeLabel: isDark }" v-if="label">{{
+      label
+    }}</label>
+    <input
+      type="text"
+      id="inputField"
+      :placeholder="name ? name : ''"
+      v-model.trim="inputValue"
+      class="input"
+      :class="{ darkThemeInput: isDark }"
+      :disabled="disabled"
+      :style="{ width: size }"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import '@/assets/styles/style.css';
 
-export default defineComponent({
-  name: 'BaseInput',
+export default {
   components: {},
-  data(): {} {
+  data(): { inputValue: string } {
     return {
       inputValue: '',
-      labelText: '',
     };
   },
-  // props: {
-
-  // },
-});
+  props: {
+    label: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    isDark: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: '100%',
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.inputBlock {
+  display: flex;
+  flex-direction: column;
+  font-family: var(--main-font-style);
+  gap: 0.5rem;
+  font-size: 1rem;
+  height: fit-content;
+}
+
+.input {
+  background: transparent;
+  border: 1px solid var(--main-purple);
+  border-radius: 3px;
+  font-size: 1rem;
+  padding: 0.1rem 0.3rem;
+}
+
+.label {
+  color: var(--main-font-color);
+}
+
+.input::placeholder {
+  color: var(--placeholder-font-color);
+  font-size: 0.8rem;
+}
+
+.darkThemeInput {
+  border: 1px solid #ffffff;
+  color: var(--dark-theme-font-color);
+}
+
+.darkThemeLabel,
+.darkThemeInput::placeholder {
+  color: var(--dark-theme-font-color);
+}
+</style>
--- src/stories/BaseInput.stories.ts
+++ src/stories/BaseInput.stories.ts
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/vue3';
 import BaseInput from '../components/shared/BaseInput.vue';
 
 const meta: Meta<typeof BaseInput> = {
+  title: 'Input',
   component: BaseInput,
 };
 
@@ -18,6 +19,22 @@ export const Primary: Story = {
     template: '<BaseInput v-bind="args" />',
   }),
   args: {
+    label: 'Type here:',
+    name: 'Add something...',
+  },
+};
 
+export const DarkMode: Story = {
+  render: (args) => ({
+    components: { BaseInput },
+    setup(): {} {
+      return { args };
+    },
+    template: '<BaseInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Type here:',
+    name: 'Add something...',
+    isDark: true,
   },
 };
