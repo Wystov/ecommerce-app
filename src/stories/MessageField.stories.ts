730be675b450334b6fc352a88dd3c1b6124ff62e
Duxcoder <iinegve_25@mail.ru>
2023-08-08 21:14:13 +0300

    feat: add stories for MessageField

---
+++ src/stories/MessageField.stories.ts
@@ -0,0 +1,76 @@
+import MessageField from '@/components/shared/MessageField.vue';
+
+export default {
+  title: 'MessageField',
+  component: MessageField,
+  tags: ['autodocs'],
+  argTypes: {
+    alert: {
+      control: {
+        type: 'select',
+      },
+      options: ['primary', 'warning', 'danger', 'success'],
+    },
+  },
+};
+
+export const Primary = {
+  args: {
+    alert: 'primary',
+    title: 'Title',
+    text: `Le Lorem Ipsum est simplement du faux texte
+    employé dans la composition et la mise en page avant
+    impression. Le Lorem Ipsum est le faux texte standard de
+    l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+    assembla ensemble des morceaux de texte pour réaliser un livre
+    spécimen de polices de texte`,
+  },
+};
+
+export const Warning = {
+  args: {
+    alert: 'warning',
+    text: `Le Lorem Ipsum est simplement du faux texte
+    employé dans la composition et la mise en page avant
+    impression. Le Lorem Ipsum est le faux texte standard de
+    l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+    assembla ensemble des morceaux de texte pour réaliser un livre
+    spécimen de polices de texte`,
+  },
+};
+
+export const Success = {
+  args: {
+    alert: 'success',
+    text: `Le Lorem Ipsum est simplement du faux texte
+    employé dans la composition et la mise en page avant
+    impression. Le Lorem Ipsum est le faux texte standard de
+    l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+    assembla ensemble des morceaux de texte pour réaliser un livre
+    spécimen de polices de texte`,
+  },
+};
+
+export const Danger = {
+  args: {
+    alert: 'danger',
+    text: `Le Lorem Ipsum est simplement du faux texte
+    employé dans la composition et la mise en page avant
+    impression. Le Lorem Ipsum est le faux texte standard de
+    l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+    assembla ensemble des morceaux de texte pour réaliser un livre
+    spécimen de polices de texte`,
+  },
+};
+
+export const Rounded = {
+  args: {
+    rounded: true,
+    text: `Le Lorem Ipsum est simplement du faux texte
+    employé dans la composition et la mise en page avant
+    impression. Le Lorem Ipsum est le faux texte standard de
+    l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+    assembla ensemble des morceaux de texte pour réaliser un livre
+    spécimen de polices de texte`,
+  },
+};
