069a99cd906861850b7931eb404a9b6550979821
Wystov <wystov@gmail.com>
2023-08-09 15:45:05 +0300

    refactor: delete redundant and commented lines

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <script lang="ts">
-import { ApiClient } from './utils/api/client';
-
-export default {
-  mounted(): void {
-    const api = new ApiClient();
-
-    // anonymous flow
-    api.call().get().execute();
-    // api.createCustomer('12467asdfadsf@asd.com', '123');
-
-    // password flow
-    // api.signInCustomer('1246asdfadsf@asd.com', '123');
-  },
-};
+export default {};
 </script>
