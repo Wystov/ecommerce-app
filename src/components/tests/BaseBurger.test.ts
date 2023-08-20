7258990811fb8097b6e8fa03884afd7b770ba1b5
Anna Coração <lazakovich@gmail.com>
2023-08-20 18:25:04 +0300

    feat: add test for burger logo

---
+++ src/components/tests/BaseBurger.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { NamePages } from '@/types/enums';
+import BaseBurger from '../shared/BaseBurger.vue';
+
+describe('Base burger logo tests', () => {
+  it('should render burger logo in closed menu state', () => {
+    const wrapper = mount(BaseBurger, {
+      props: {
+        open: false,
+      },
+    });
+    expect(wrapper.find('div.burger').exists()).toBeTruthy();
+    const burgerLines = wrapper.findAll('.burger-line');
+    expect(burgerLines).toHaveLength(3);
+  });
+
+  it('should render burger logo in open menu state', () => {
+    const wrapper = mount(BaseBurger, {
+      props: {
+        open: true,
+      },
+    });
+    expect(wrapper.find('div.burger-closed').exists()).toBeTruthy();
+    const burgerLines = wrapper.findAll('.close-line');
+    expect(burgerLines).toHaveLength(2);
+  });
+});
