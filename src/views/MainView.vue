599fdad53492c725035102ff96f5515b5f60c861
Anna Coração <lazakovich@gmail.com>
2023-08-20 15:09:25 +0300

    feat: add adaptive version of main page

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -13,18 +13,23 @@
           />
         </div>
         <div class="hero-image">
-          <img src="@/assets/images/hero-image.svg" alt="hero" />
+          <img src="@/assets/images/hero-image.svg" alt="hero" class="hero-img" />
         </div>
       </div>
-      <div class="promo hero-text-container">
-        <h1 class="title">Promos</h1>
-        <p class="description">Enjoy special promocodes for registered customers</p>
-        <BaseButton
-          @click="$router.push({ name: 'Sign Up' })"
-          size="large"
-          class="button"
-          label="Explore"
-        />
+      <div class="promo">
+        <div class="promo-text-container">
+          <h1 class="title">Promos</h1>
+          <div class="promo-image">
+            <img src="@/assets/images/promo.svg" alt="promo label" class="promo-img" />
+          </div>
+          <p class="description">Enjoy special promocodes for registered customers</p>
+          <BaseButton
+            @click="$router.push({ name: 'Sign Up' })"
+            size="large"
+            class="button"
+            label="Join us"
+          />
+        </div>
       </div>
     </div>
   </main>
@@ -40,30 +45,33 @@ export default {
 
 <style scoped>
 .first-screen {
-  display: flex;
-  align-items: stretch;
-  gap: 40px;
+  display: grid;
+  grid-template-columns: 62.5% 35%;
+  justify-content: space-between;
+  justify-items: stretch;
+  width: 100%;
   margin-top: 50px;
 }
 .hero {
-  width: 85%;
+  display: flex;
   padding: 50px 75px;
   border-radius: 10px;
   background-color: var(--secondary-color);
 }
-.hero-text-container,
-.promo {
+.hero-text-container {
   display: flex;
   flex-direction: column;
   gap: 35px;
-}
-.hero-text-container {
   width: 70%;
+}
+.hero-img {
+  width: 100%;
 }
 .title {
   color: white;
   font-size: 62px;
   font-weight: 600;
+  margin: 0;
 }
 .description {
   color: white;
@@ -75,10 +83,70 @@ export default {
   color: var(--main-font-color);
 }
 .promo {
-  flex-grow: 1;
-  height: 100%;
-  padding: 50px 75px;
+  position: relative;
   border-radius: 10px;
   background-color: var(--main-color);
+}
+.promo-text-container {
+  padding: 50px 75px;
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+}
+.promo-image {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+}
+@media (max-width: 1500px) {
+  .title {
+    font-size: 3rem;
+  }
+  .description {
+    font-size: 1.6rem;
+  }
+  .hero,
+  .promo-text-container {
+    padding: 30px 50px;
+  }
+}
+@media (max-width: 1100px) {
+  .first-screen {
+    grid-template-columns: 100%;
+    gap: 20px;
+  }
+  .promo {
+    width: 100%;
+  }
+  .hero-image {
+    width: 25%;
+  }
+  .promo-image {
+    width: 70px;
+  }
+  .promo-img {
+    width: 100%;
+  }
+}
+@media (max-width: 750px) {
+  .title {
+    font-size: 2rem;
+  }
+  .description {
+    font-size: 1rem;
+  }
+  .hero-image {
+    display: none;
+  }
+  .hero-text-container {
+    width: 100%;
+  }
+  .button {
+    font-size: 1.5rem;
+  }
+  .promo-image {
+    top: auto;
+    bottom: 20px;
+  }
 }
 </style>
