ceeed7290cb984e5752da76b618c18b5b7ff7ecd
Wystov <wystov@gmail.com>
2023-08-21 06:01:40 +0300

    feat: redirect authorized users from login and registration pages

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,11 +3,23 @@
 </template>
 
 <script lang="ts">
+import { mapStores } from 'pinia';
+import { useUserStore } from '@/stores/user';
 import AppLogin from '@/components/AppLoginForm.vue';
 
 export default {
   components: {
     AppLogin,
+  },
+  computed: {
+    ...mapStores(useUserStore),
+  },
+  created(): void {
+    if (this.userStore.fetching) {
+      this.$watch(() => this.userStore.fetching, () => {
+        if (this.userStore.authorized) this.$router.push({ name: 'Home' });
+      });
+    }
   },
 };
 </script>
