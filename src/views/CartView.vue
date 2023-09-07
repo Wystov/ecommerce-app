ef9a76f1f03b304b5c4077c1619b0300d6caafc6
Wystov <wystov@gmail.com>
2023-09-07 19:21:11 +0300

    feat: display cart items

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,11 +1,136 @@
 <template>
-  <h1 class="wrapper">
-    Cart
-  </h1>
+  <div class="wrapper">
+    <h1>Cart</h1>
+    <div v-if="data">
+      <ul class="cart-item-list">
+        <li
+          v-for="item in data.lineItems"
+          :key="item.id"
+          class="cart-item">
+          <RouterLink
+            class="item-image"
+            :to="{ name: 'Product', params: { slug: item.productSlug?.en } }">
+            <img
+              :src="item.variant.images?.[0].url ?? imgPlaceholder"
+              alt="item image"
+              class="item-image" />
+          </RouterLink>
+          <div class="item-content">
+            <div class="item-info">
+              <div class="item-name">
+                {{ item.name.en }}
+              </div>
+              <div>
+                Price:
+                <template v-if="item.price.discounted">
+                  <span class="old-price">
+                    {{ formattedPrice(item.price.value.centAmount) }}
+                  </span>
+                  <span class="new-price">
+                    {{ formattedPrice(item.price.discounted.value.centAmount) }}
+                  </span>
+                </template>
+                <template v-else>
+                  <span class="price">
+                    {{ formattedPrice(item.price.value.centAmount) }}
+                  </span>
+                </template>
+              </div>
+            </div>
+            <div class="item-values">
+              <div>-{{ item.quantity }}+</div>
+              <div class="item-total">
+                {{ formattedPrice(item.totalPrice.centAmount) }}
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import type { Cart } from '@commercetools/platform-sdk';
+import { mapState } from 'pinia';
+import { useUserStore } from '@/stores/user';
+import api from '@/utils/api/client';
+import imgPlaceholder from '@/assets/images/no-image-placeholder.svg';
+
 export default {
-  components: {},
+  data(): { imgPlaceholder: string; data: Cart | null } {
+    return {
+      imgPlaceholder,
+      data: null,
+    };
+  },
+  computed: {
+    ...mapState(useUserStore, ['currencyTag']),
+  },
+  methods: {
+    async getCart(): Promise<void> {
+      const response = await api.call().me().activeCart().get().execute();
+      console.log(response);
+      this.data = response.body;
+    },
+    formattedPrice(price: number): string {
+      return `${this.currencyTag}${(price / 100).toFixed(2)}`;
+    },
+  },
+  created(): void {
+    this.getCart();
+  },
 };
 </script>
+
+<style>
+.wrapper {
+  width: 100%;
+}
+.cart-item {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  padding-right: 6rem;
+  gap: 1rem;
+  margin-bottom: 1.25rem;
+  border: 1px solid #e9e9e9;
+  border-radius: 10px;
+}
+.item-image {
+  display: block;
+  max-width: 120px;
+}
+.item-content {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
+.item-name {
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+.old-price {
+  text-decoration: line-through;
+}
+.price,
+.new-price {
+  font-size: 1rem;
+}
+.new-price {
+  padding-left: 0.5rem;
+  color: var(--main-color);
+}
+.item-values {
+  display: flex;
+  align-items: center;
+}
+.item-total {
+  font-size: 1.25rem;
+  text-align: right;
+  min-width: 8rem;
+}
+</style>
