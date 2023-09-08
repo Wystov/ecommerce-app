7843bfc967e137bd6fc089853e21eeb77c629ede
Duxcoder <iinegve_25@mail.ru>
2023-09-08 19:34:29 +0200

    feat: add delete product, and refactor

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import type { LineItem } from '@commercetools/platform-sdk';
+import type { LineItem, MyCartUpdateAction } from '@commercetools/platform-sdk';
 import api from '@/utils/api/client';
 import type { ProductCart, StateCart } from '@/types/types';
 
@@ -18,31 +18,30 @@ export const useCartStore = defineStore('cart', {
     },
   },
   actions: {
+
     async createCart() {
-      const data = await api.call().me().carts().post({ body: { country: 'US', currency: 'USD' } }).execute();
+      const queryArgs = { body: { country: 'US', currency: 'USD' } };
+      const data = await api.call().me().carts().post(queryArgs).execute();
       this.cartId = data.body.id;
       this.cartVersion = data.body.version;
-      api.call().me().carts().get().execute().then((res) => console.log('create cart', res));
     },
+
     hasProductInCart(keyProduct: number): Boolean {
       return this.products.some((product) => product.keyProduct === keyProduct);
     },
+
     async addProductToCart(keyProduct: number, skuProduct: string) {
       if (this.hasProductInCart(keyProduct)) {
         const index = this.products.findIndex((product) => product.keyProduct === keyProduct);
-        console.log('product has');
         this.increaseCount(index);
-        console.log('increase complete');
         await this.updateQuantity(index);
-        console.log('update quantity complete');
       } else {
         const product: ProductCart = { keyProduct, count: 1, sku: skuProduct };
         this.products.push(product);
-        console.log('product push', skuProduct, this.cartVersion);
         await this.sendProduct(product);
-        console.log('product send');
       }
     },
+
     async removeProductFromCart(keyProduct: number, all = false) {
       const index = this.products.findIndex((product) => product.keyProduct === keyProduct);
       if (this.hasProductInCart(keyProduct) && this.products[index].count > 1 && !all) {
@@ -53,63 +52,75 @@ export const useCartStore = defineStore('cart', {
         this.products.splice(index, 1);
       }
     },
+
     increaseCount(index: number) {
       this.products[index].count += 1;
     },
+
     decreaseCount(index: number) {
       this.products[index].count -= 1;
     },
+
     getCountProduct(keyProduct: number) {
       const product = this.products.find((productData) => productData.keyProduct === keyProduct);
       return product ? product.count : 0;
     },
+
     async sendProduct(productCart: ProductCart) {
       const findProduct = (p: ProductCart): boolean => p.keyProduct === productCart.keyProduct;
       const index = this.products.findIndex(findProduct);
-      console.log(this.products[index], index);
-      const cart = await api.call().me().carts().withId({ ID: this.cartId }).post({ body: {
-        version: this.cartVersion || 1,
-        actions: [
-          {
-            action: 'addLineItem',
-            sku: this.products[index].sku,
-          },
-        ],
-      } }).execute();
+      const action: MyCartUpdateAction = {
+        action: 'addLineItem',
+        sku: this.products[index].sku,
+      };
+      const queryArgs = {
+        body: {
+          version: this.cartVersion || 1,
+          actions: [action],
+        },
+      };
+      const cart = await api.call().me().carts().withId({ ID: this.cartId })
+        .post(queryArgs).execute();
       const keyProduct = this.products[index].keyProduct.toString();
       const findLineItem = (item: LineItem): boolean => item.productKey === keyProduct;
       const lineItem = cart.body.lineItems.find(findLineItem);
       this.products[index].lineItemId = lineItem?.id;
       this.cartVersion = cart.body.version;
-      console.log('add prod', cart, this.cartVersion);
     },
+
     async updateQuantity(indexProduct: number) {
-      const cart = await api.call().me().carts().withId({ ID: this.cartId }).post({ body: {
-        version: this.cartVersion || 1,
-        actions: [
-          {
-            action: 'changeLineItemQuantity',
-            lineItemId: this.products[indexProduct].lineItemId,
-            quantity: this.products[indexProduct].count,
-          },
-        ],
-      } }).execute();
+      const action: MyCartUpdateAction = {
+        action: 'changeLineItemQuantity',
+        lineItemId: this.products[indexProduct].lineItemId,
+        quantity: this.products[indexProduct].count,
+      };
+      const queryArgs = {
+        body: {
+          version: this.cartVersion || 1,
+          actions: [action],
+        },
+      };
+      const cart = await api.call().me().carts()
+        .withId({ ID: this.cartId }).post(queryArgs).execute();
       this.cartVersion = cart.body.version;
-      console.log('update prod', cart);
     },
+
     async deleteProduct(indexProduct: number) {
-      const cart = await api.call().me().carts().withId({ ID: this.cartId }).post({ body: {
-        version: this.cartVersion || 1,
-        actions: [
-          {
-            action: 'removeLineItem',
-            lineItemId: this.products[indexProduct].lineItemId,
-          },
-        ],
-      } }).execute();
+      const action: MyCartUpdateAction = {
+        action: 'removeLineItem',
+        lineItemId: this.products[indexProduct].lineItemId,
+      };
+      const queryArgs = {
+        body: {
+          version: this.cartVersion || 1,
+          actions: [action],
+        },
+      };
+      const cart = await api.call().me().carts()
+        .withId({ ID: this.cartId }).post(queryArgs).execute();
       this.cartVersion = cart.body.version;
-      console.log('delete prod', cart);
     },
+
     async deleteCart() {
       await api.call().me().carts().withId({ ID: this.cartId })
         .delete({ queryArgs: { version: 1 } }).execute();
